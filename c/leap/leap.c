7b6404292809835e346c048ce9db4e82d8cf84d2
Blxee <aminedev57@gmail.com>
2025-09-06 16:17:21 +0100

    Added: leap in c

---
+++ c/leap/leap.c
@@ -0,0 +1,10 @@
+#include "leap.h"
+
+bool leap_year(int year)
+{
+  bool div4 = year % 4 == 0;
+  bool div100 = year % 100 == 0;
+  bool div400 = year % 400 == 0;
+
+  return div4 && (!div100 || div400);
+}
