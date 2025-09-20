f04b57a62f6ea017c996774e43802058e14b17f1
Blxee <aminedev57@gmail.com>
2025-09-20 18:11:44 +0100

    Added: prime-factors in rust (optimized)

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,24 +1,13 @@
 pub fn factors(mut n: u64) -> Vec<u64> {
-    let mut primes = Vec::new();
-
-    for i in 2..=n {
-        let sqrt = (i as f32).sqrt() as u64;
-        let is_prime = primes
-            .iter()
-            .take_while(|&&j| j <= sqrt)
-            .all(|&j| i % j != 0);
-        if is_prime {
-            primes.push(i);
-        }
-    }
-
     let mut facts = Vec::new();
 
-    for p in primes {
-        while n != 0 && n % p == 0 {
-            facts.push(p);
-            n /= p;
+    let mut i = 2;
+    while i <= n {
+        while n != 0 && n % i == 0 {
+            facts.push(i);
+            n /= i;
         }
+        i += 1;
     }
 
     facts
