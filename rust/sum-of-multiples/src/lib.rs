dbf24e29052547a1bcf604f8e3e5d75af5300c8a
Blxee <aminedev57@gmail.com>
2025-09-22 13:10:50 +0100

    Changed: sum-of-multiples in rust

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,14 +1,5 @@
-use std::collections::HashSet;
-
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    factors
-        .into_iter()
-        .fold(HashSet::new(), |mut set, &val| {
-            if val > 0 {
-                set.extend((val..limit).step_by(val as usize));
-            }
-            set
-        })
-        .into_iter()
+    (0..limit)
+        .filter(|&i| factors.iter().any(|&v| v != 0 && i % v == 0))
         .sum()
 }
