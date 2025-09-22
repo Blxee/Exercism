47203f744ba030cde3f1bec0dc3f6060e619efcf
Blxee <aminedev57@gmail.com>
2025-09-22 15:21:41 +0100

    Changed: gigasecond in c (now using strftime)

--- c/gigasecond/gigasecond.c
+++ c/gigasecond/gigasecond.c
@@ -7,12 +7,5 @@ void gigasecond(time_t input, char *output, size_t size)
 	(void)size;
 	input += 1000000000;
 	date = gmtime(&input);
-	sprintf(output, "%d%02d-%02d-%02d %02d:%02d:%02d",
-			date->tm_year / 100 == 1 ? 20 : 19,
-			date->tm_year % 100,
-			date->tm_mon + 1,
-			date->tm_mday,
-			date->tm_hour,
-			date->tm_min,
-			date->tm_sec);
+	strftime(output, size, "%Y-%m-%d %H:%M:%S", date);
 }
