7fe85bb6282ae7ee4f51efc0c3fcda1f1fecc03e
Blxee <aminedev57@gmail.com>
2025-09-26 15:08:32 +0100

    Changed: kindergarten-garden in rust

--- rust/kindergarten-garden/src/lib.rs
+++ rust/kindergarten-garden/src/lib.rs
@@ -1,22 +1,17 @@
-use std::collections::HashMap;
-
 pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
-    let plants: HashMap<char, &'static str> = HashMap::from_iter([
-        ('G', "grass"),
-        ('C', "clover"),
-        ('R', "radishes"),
-        ('V', "violets"),
-    ]);
+    let get_plant = |c| match c {
+        'G' => "grass",
+        'C' => "clover",
+        'R' => "radishes",
+        'V' => "violets",
+        _ => panic!("No such plant!"),
+    };
 
-    let get_student_pos = |student: &str| (student.as_bytes()[0] - b'A') as usize;
+    let student_pos = (student.as_bytes()[0] - b'A') as usize;
 
-    let mut results = Vec::new();
-
-    diagram.split('\n').for_each(|row| {
-        let mut iter = row.chars().skip(get_student_pos(student) * 2);
-        results.push(*plants.get(&iter.next().unwrap()).unwrap());
-        results.push(*plants.get(&iter.next().unwrap()).unwrap());
-    });
-
-    results
+    diagram
+        .lines()
+        .flat_map(|row| row.chars().skip(student_pos * 2).take(2))
+        .map(get_plant)
+        .collect()
 }
